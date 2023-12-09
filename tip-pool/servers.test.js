cd7af4ff400001e468e5d79aec94d007389c08e4
patel <parthpatel167@gmail.com>
2023-12-09 17:30:09 -0600

    create test for server add some js code add all test pass

--- tip-pool/servers.test.js
+++ tip-pool/servers.test.js
@@ -1,4 +1,4 @@
-describe("Servers test (with setup and tear-down)", function() {
+describe('Servers test (with setup and tear-down)', function () {
   beforeEach(function () {
     // initialization logic
     serverNameInput.value = 'Alice';
@@ -10,8 +10,27 @@ describe("Servers test (with setup and tear-down)", function() {
     expect(Object.keys(allServers).length).toEqual(1);
     expect(allServers['server' + serverId].serverName).toEqual('Alice');
   });
+  // alteast one server needed
+  it('should not  add a new server with empty value to allServers on submitServerInfo()', function () {
+    serverNameInput.value = '';
+    submitServerInfo();
+    expect(Object.keys(allServers).length).toEqual(0);
+  });
+  it('should update table after updateServerTable()', function () {
+    updateServerTable();
+    submitServerInfo();
+    let tdList = document.querySelectorAll('#serverTable tbody tr td');
 
-  afterEach(function() {
+    expect(tdList.length).toEqual(2);
+    expect(tdList[0].innerText).toEqual('Alice');
+    expect(tdList[1].innerText).toEqual('$0.00');
+  });
+  //
+
+  afterEach(function () {
     // teardown logic
+    serverId = 0;
+    serverTbody.innerHTML = '';
+    allServers = {};
   });
 });
